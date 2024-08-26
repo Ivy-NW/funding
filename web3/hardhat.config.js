d32aa465fedb11fb47657a0cca8401db5f6d3937
KODAK <ivynjoroge30@gmail.com>
2024-08-26 17:17:19 +0300

    contract connection

---
+++ web3/hardhat.config.js
@@ -0,0 +1,49 @@
+require("@matterlabs/hardhat-zksync-solc");
+require("@nomiclabs/hardhat-ethers"); // Import ethers if you are using it
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  zksolc: {
+    version: "1.3.9",
+    compilerSource: "binary",
+    settings: {
+      optimizer: {
+        enabled: true,
+      },
+    },
+  },
+  networks: {
+    zksync_testnet: {
+      url: "https://zksync2-testnet.zksync.dev",
+      ethNetwork: "sepolia",
+      chainId: 280,
+      zksync: true,
+    },
+    zksync_mainnet: {
+      url: "https://zksync2-mainnet.zksync.io/",
+      ethNetwork: "mainnet",
+      chainId: 324,
+      zksync: true,
+    },
+    hardhat: {},
+    sepolia: {
+      url: 'https://rpc.ankr.com/eth_sepolia',
+      accounts: ["9b96978608450de9aa5a1070a1bed36c27fb21984c5e88c544dd0e1072bf778f"], // Use environment variable for security
+    },
+  },
+  paths: {
+    artifacts: "./artifacts-zk",
+    cache: "./cache-zk",
+    sources: "./contracts",
+    tests: "./test",
+  },
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
